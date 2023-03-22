b7c88006f3c831e12ed9aaf881767ff41415a786
Joey West <120219149+JWLMT88@users.noreply.github.com>
2023-03-22 18:52:28 +0100

    adding tests

--- Tests.cs
+++ Tests.cs
@@ -1,13 +1,62 @@
-﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Xamarin.Forms;
 
-namespace LILO.Shell;
-internal class Tests
+namespace LILO.Shell.Tests
 {
-    
+    [TestClass]
+    public class ShellExtentionsTests
+    {
+        [TestMethod]
+        public void ShowError_ShouldShowErrorMessage()
+        {
+            // Arrange
+            string message = "An error occurred";
+
+            // Act
+            ShellExtentions.ShowError(message);
+
+            // Assert
+            Assert.AreEqual($"Sorry, something went wrong : {message}", message);
+        }
+
+        [TestMethod]
+        public void MediaExtentions_EncryptAudioFile_ShouldSucceed()
+        {
+            // Arrange
+            string path = "C:\\audiofile.txt";
+            string key = "secretkey";
+
+            // Act
+            ShellExtentions.MediaExtentions.EncryptAudioFile(path, key);
+
+            //Assert
+            string expectedfilename = Path.GetFileNameWithoutExtension(path) + ".laf";
+            string actualfilename = Path.Combine(Path.GetDirectoryName(path), expectedfilename);
+            FileInfo fileinfo = new FileInfo(actualfilename);
+            Assert.IsTrue(fileinfo.Exists);
+        }
+
+        [TestMethod]
+        public void MediaExtentions_DecryptAudioFile_ShouldSucceed()
+        {
+            // Arrange
+            string path = "C:\\audiofile.laf";
+            string key = "secretkey";
+
+            // Act
+            ShellExtentions.MediaExtentions.DecryptAudioFile(path, key);
+            
+            // Assert
+            string expectedfilename = Path.GetFileNameWithoutExtension(path) + ".mp3";
+            string actualfilename = Path.Combine(Path.GetDirectoryName(path), expectedfilename);
+            FileInfo fileinfo = new FileInfo(actualfilename);
+            Assert.IsTrue(fileinfo.Exists);
+        }
+    }
 }
